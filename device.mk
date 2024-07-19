9278d3130939d4d53c365e816a53a39f5ec1e4d4
LMO GerritBot <gerrit@libremobileos.com>
2024-07-19 06:35:17 +0000

    Adapt to LMODroid by device-add script

--- device.mk
+++ device.mk
@@ -25,7 +25,7 @@ $(call inherit-product, device/lge/sdm845-common/sdm845.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay \
-    $(DEVICE_PATH)/overlay-lineage
+    $(DEVICE_PATH)/overlay-lmodroid
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
